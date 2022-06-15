44cfc0e7087497cf8c2599ede4a995a1047679d0
jenCkw <cikwaninejenny@gmail.com>
2022-06-15 17:04:06 +0100

    concatenation

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 //Les methodes
 //1.indexof
-let nom = 'jenny';
-console.log(nom.indexOf('y'));
+//let nom = 'jenny';
+//console.log(nom.indexOf('y'));
+
+let nom = "jenny";
+let postnom = "Chikwanine"
+console.log(nom, postnom);
+
+let nom_complet = nom + " " + postnom;
+
+console.log(nom_complet, postnom);
