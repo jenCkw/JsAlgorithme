51d42a97722cbd7a7ec8e707c69b87e2b15a27e4
jenCkw <cikwaninejenny@gmail.com>
2022-06-25 21:43:27 +0100

    exo algojs

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 //console.log(nom.indexOf('y'));
 
 //Les varaibles
-let nom = "jenny";
+// let nom = "jenny";
 // let postnom = "Chikwanine"
 // console.log(nom, postnom);
 
@@ -12,7 +12,30 @@ let nom = "jenny";
 
 // console.log(nom_complet, postnom);
 
-let lgr = nom.length;
-console.log(lgr);
-console.log(nom.toUpperCase());
-console.log(nom.toLowerCase());
+//---------------toUppercase----------
+// let lgr = nom.length;
+// console.log(lgr);
+// console.log(nom.toUpperCase());
+// console.log(nom.toLowerCase());
+
+
+//Les methodes en js
+//-----indexof
+
+// let nom = 'jenny';
+// console.log(nom.indexOf('y'));
+
+//-----slice
+
+// let nom = jenny;
+// console.log(nom.slice(3,5));
+
+//Exercice1
+
+
+//----methode substr
+
+let nom = 'jenny';
+console.log(nom.substr(0, 4));
+
+console.log(nom.typeof);
