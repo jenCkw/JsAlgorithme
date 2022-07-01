5596ac834705c6c768396cb3ce8e43fc5da44696
jenCkw <cikwaninejenny@gmail.com>
2022-07-01 16:39:40 +0100

    ajout fonctions chekguess

--- script.js
+++ script.js
@@ -11,6 +11,30 @@ let guessField = document.querySelector('.guessField');
 let guessCount = 1;
 let resetButton;
 
-// function checkGuess() {
-//     alert('je suis un espace réservé')
-// }
+function checkGuess() {
+    let userGuess =  Number(guessField.value);
+    if (userGuess === 1){
+        guesses.textContent = 'Propositions précédentes&nbsp;: ';
+    }
+
+    guesses.textContent += userGuess + ' ';
+
+
+    if (userGuess === randomNumber) {
+        lastResult.textContent = 'Brovo, vous avez trouvé le nombre';
+        lastResult.style.backgroundColor = 'green';
+        lowOrHi.textContent = '';
+        setGameOver();
+    } else if (guessCount === 10){
+        lastResult.textContent = 'Faux&nbsp;!';
+        lastResult.style.backgroundColor = 'red';
+        
+        if (userGuess < randomNumber){
+            lowOrHi.textContent =  'Le nombre saisi est trop grand !';
+        }
+    }
+
+    guessCount++;
+    guessField.value = '';
+    guessField.focus();
+}
