056d40311971de89b0ca02c5be57ec42d58ea78a
jenCkw <cikwaninejenny@gmail.com>
2022-07-09 18:23:50 +0100

    guess game updated

--- script.js
+++ script.js
@@ -1,46 +1,75 @@
 // generer un nombre aleatoire .
 
-// let randomNumber = Math.floor(Math.random() * 100) + 1; //reçoit le nombre aléatoire entre 1 et 100, calculé en utilisant un algorithme mathématique.
+let randomNumber = Math.floor(Math.random() * 100) + 1; //reçoit le nombre aléatoire entre 1 et 100, calculé en utilisant un algorithme mathématique.
 
-// let guesses = document.querySelector('.guesses');
-// let lastResult = document.querySelector('.lastResult');
-// let lowOrHi = document.querySelector('.lowOrHi');
+let guesses = document.querySelector(".guesses");
+let lastResult = document.querySelector(".lastResult");
+let lowOrHi = document.querySelector(".lowOrHi");
 
-// let guessSubmit = document.querySelector('.guessSubmit');
-// let guessField = document.querySelector('.guessField');
+let guessSubmit = document.querySelector(".guessSubmit");
+let guessField = document.querySelector(".guessField");
 
-// let guessCount = 1;
-// let resetButton;
+let guessCount = 1;
+let resetButton;
 
-// function checkGuess(){
-//     let userGuess = Number(guessField.value);
-//     if (guessCount === 1) {
-//       guesses.textContent = 'Propositions précédentes&nbsp;: ';
-//     }
-//     guesses.textContent += userGuess + ' ';
+function checkGuess() {
+  let userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = "Propositions précédentes&nbsp;: ";
+  }
+  guesses.textContent += userGuess + " ";
 
-//     if (userGuess === randomNumber) {
-//       lastResult.textContent = 'Bravo, vous avez trouvé le nombre !';
-//       lastResult.style.backgroundColor = 'green';
-//       lowOrHi.textContent = '';
-//       setGameOver();
-//     } else if (guessCount === 10) {
-//        lastResult.textContent = '!!! PERDU&nbsp;!!!';
-//        setGameOver();
-//     } else {
-//        lastResult.textContent = 'Faux&nbsp;!';
-//        lastResult.style.backgroundColor = 'red';
-//        if (userGuess < randomNumber) {
-//         lowOrHi.textContent = 'Le nombre saisi est trop petit !';
-//        } else if (userGuess > randomNumber) {
-//         lowOrHi.textContent = 'Le nombre saisi est trop grand !';
-//        }
-//     }
+  if (userGuess === randomNumber) {
+    lastResult.textContent = "Bravo, vous avez trouvé le nombre !";
+    lastResult.style.backgroundColor = "green";
+    lowOrHi.textContent = "";
+    setGameOver();
+  } else if (guessCount === 10) {
+    lastResult.textContent = "!!! PERDU&nbsp;!!!";
+    setGameOver();
+  } else {
+    lastResult.textContent = "Faux&nbsp;!";
+    lastResult.style.backgroundColor = "red";
+    if (userGuess < randomNumber) {
+      lowOrHi.textContent = "Le nombre saisi est trop petit !";
+    } else if (userGuess > randomNumber) {
+      lowOrHi.textContent = "Le nombre saisi est trop grand !";
+    }
+  }
 
-//     guessCount++;
-//     guessField.value = '';
-//     guessField.focus();
-//   }
+  guessCount++;
+  guessField.value = "";
+  guessField.focus();
+}
+guessSubmit.addEventListener("click", checkGuess);
+function setGameOver() {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement("button");
+  resetButton.textContent = "Start new game";
+  document.body.appendChild(resetButton);
+  resetButton.addEventListener("click", resetGame);
+}
+
+function resetGame() {
+  guessCount = 1;
+
+  let resetParas = document.querySelectorAll(".resultParas p");
+  for (let i = 0; i < resetParas.length; i++) {
+    resetParas[i].textContent = "";
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = "";
+  guessField.focus();
+
+  lastResult.style.backgroundColor = "white";
+
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
 
 //produit de deux nombres
 // function myFunction(a, b) {
