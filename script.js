62cdf00f908001c6efd7d8a858d1bb1be418c5c3
jenCkw <cikwaninejenny@gmail.com>
2022-07-13 12:54:15 +0100

    methode pop sur un tableau

--- script.js
+++ script.js
@@ -15,4 +15,6 @@ let episodes = [firstEpisode, secondEpisode, thirdEpisode];
 episodes.push("4emEpisode");
 episodes.push("episode5", "episode6");
 episodes.unshift("episode5", "episode6", "episode7");
+
+episodes.pop();
 console.log(episodes.length);
