5530110d9003f000c31fba24d9137514716de4c5
jenCkw <cikwaninejenny@gmail.com>
2022-07-17 13:21:11 +0100

    boucle for

--- app.js
+++ app.js
@@ -1,36 +1,6 @@
-let firstUser = {
-  name: "Will Alexander",
-  age: 33,
-  accountLevel: "normal",
-};
-
-let secondUser = {
-  name: "Sarah Kate",
-  age: 21,
-  accountLevel: "premium",
-};
-
-let thirdUser = {
-  name: "Audrey Simon",
-  age: 27,
-  accountLevel: "mega-premium",
-};
-
-switch (firstUser.accountLevel) {
-  case "normal":
-    console.log("you have a normal account");
-
-    break;
-
-  case "premium":
-    console.log("You have a premium account!");
-
-    break;
-
-  case "mega-premium":
-    console.log("You have a mega premium account!");
-    break;
-
-  default:
-    console.log("Unknown account type");
+const numberOfPassengers = 10;
+for (let i = 0; i < numberOfPassengers; i++) {
+  console.log("Passager embarqué !");
 }
+
+console.log("Tous les passagers sont embarqués !");
