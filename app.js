4a798b3ab5ba6d255d7eb97f65893e0947720efb
jenCkw <cikwaninejenny@gmail.com>
2022-07-17 12:21:29 +0100

    instructions conditionnelles avec if et else 2

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
+//on pointe sur l'element de message
+const espaceMessage = document.getElementById("message");
+//On pointe sur l'element du boutton
+const bouton = document.getElementById("bouton");
+//On pointe sur l'élément de chmaps de saisie
+const ageInput = document.getElementById("age");
+//On pointe sur l'élément de checkbox de controle
+const parentalInput = document.getElementById("parental");
+
 let age;
+let isControlParentalActive;
 let ageMajorite = 18;
 
-//cette fonction affichera le message de validation
+//Cette fonction affichera un message de validation
 function valider() {
-  espaceMessage.innerHTML = "Vous etes majeure";
+  espaceMessage.innerHTML = "Vous etes autorisé à entrer";
 }
 
-//cette fonction affichera un message d'erreur
+//Cette fonction affichera un message d'ereur
 function refuser() {
-  alert("Vous n'etes pas majeure");
+  alert("cette espace est interdit aux personnes mineurs");
 }
 
-function confirm() {
-  //On recupere la saisie de l'age et on transforme le texte en nombre entier
+function onConfirm() {
+  //On récupère la saisie de l"age et on transforme le texte en nombre entier
   age = parseInt(ageInput.value);
-
-  // si la saisie n'est pas un nombre, on affiche un message d'erreur
+  //si la saisie n'est pas un nombre, on affiche un message d'erreur
+  //Si la saisie n'est pas un nombre, on affiche un message d'erreur
   if (isNaN(age)) {
     alert("ceci n'est pas un nombre");
     return;
   }
 
-  //======Code corrigé========
+  //On recupere la valeur du chkekboc de control parental. "true" si coché ou "false" si décoché
+  isControlParentalActive = parentalInput.ariaChecked;
 
-  if (age < ageMajorite) {
+  //=====Code corrigé=====
+  if (age < ageMajorite && isControlParentalActive) {
     refuser();
   } else {
     valider();
   }
-
-  //code corrigé
-
-  //On vide le champs de saisie
-  ageInput.value = "";
 }
-
-bouton.addEventLister("click", onConfirm);
--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+//On pointe sur l'élément de message
+const espaceMessage = document.getElementById("message");
+//On pointe sur l'élément de bouton
+const bouton = document.getElementById("bouton");
+//On pointe sur l'élément de champ de saisie de l'age
+const ageInput = document.getElementById("age");
+//On pointe sur l'élément de la checkbox de contrôle parental
+const parentalInput = document.getElementById("parental");
+//On défini la variage age qu'on utilisera et un variable définissant l'age de la majorité
 let age;
+let isControlParentalActive;
 let ageMajorite = 18;
 
 //Cette fonction affichera le message de validation
@@ -19,9 +29,11 @@ function onConfirm() {
     alert("Ceci n'est pas un nombre");
     return;
   }
+  //On récupère la valeur du checkbox de contrôle parentale. "true" si coché ou "false" si décoché
+  isControlParentalActive = parentalInput.checked;
 
   //=======Code corrigé======
-  if (age < ageMajorite) {
+  if (age < ageMajorite && isControlParentalActive) {
     refuser();
   } else {
     valider();
