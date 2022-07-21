d9d7e914aa996b459b9685d10bf96ce2c21e411b
jenCkw <cikwaninejenny@gmail.com>
2022-07-21 16:32:40 +0100

    deux exemple pour ilustrer comment eviter de se repeter

--- app.js
+++ app.js
@@ -1,85 +1,70 @@
 class Show {
-  constructor(title, numberOfSeasons) {
+  constructor(title, numberOfSeasons, episodesPerSeason) {
     this.title = title;
     this.numberOfSeasons = numberOfSeasons;
-    this.ratings = [];
-    this.averageRating = 0;
-  }
-
-  addRating(rating) {
-    this.ratings.push(rating);
-    let sum = 0;
-    for (let rating of this.ratings) {
-      sum += rating;
-    }
-
-    this.averageRating = sum / this.ratings.length;
+    this.episodesPerSeason = episodesPerSeason;
   }
 }
 
+const tau = new Show("The Story of Tau", 5, 12);
+const meldrum = new Show("The Hero of Old Meldrum", 3, 6);
+const clara = new Show("The Bugs of Isla Clara", 6, 15);
+
+const shows = [tau, meldrum, clara];
+
+// Modify the following code
 // ======================
 
-const tau = new Show("The Story of Tau", 5);
-const colin = new Show("The Hero of Old Meldrum", 3);
-const clara = new Show("The Bugs of Isla Clara", 6);
+const tauTitleText = tau.title;
+const tauSeasonsText = tau.numberOfSeasons + " seasons";
+const tauEpisodesText = tau.episodesPerSeason + " episodes per season";
+const tauComponent = {
+  titleText: tauTitleText,
+  seasonsText: tauSeasonsText,
+  episodesText: tauEpisodesText,
+};
 
-const shows = [tau, colin, clara];
+const meldrumTitleText = meldrum.title;
+const meldrumSeasonsText = meldrum.numberOfSeasons + " seasons";
+const meldrumEpisodesText = meldrum.episodesPerSeason + " episodes per season";
+const meldrumComponent = {
+  titleText: meldrumTitleText,
+  seasonsText: meldrumSeasonsText,
+  episodesText: meldrumEpisodesText,
+};
+
+const claraTitleText = clara.title;
+const claraSeasonsText = clara.numberOfSeasons + " seasons";
+const claraEpisodesText = clara.episodesPerSeason + " episodes per season";
+const claraComponent = {
+  titleText: claraTitleText,
+  seasonsText: claraSeasonsText,
+  episodesText: claraEpisodesText,
+};
+
+const showComponents = [tauComponent, meldrumComponent, claraComponent];
+
+// ======================
+// Modify the code above
 
 const body = document.querySelector("body");
-const refresh = document.querySelector("#refresh");
-
-refresh.addEventListener("click", () => {
-  removeShows();
-  addRandomRatings();
-  updateShows();
-});
 
 const updateShows = () => {
-  for (let show of shows) {
+  for (let show of showComponents) {
     const showPane = document.createElement("div");
     showPane.classList.add("series-frame");
     const showHeading = document.createElement("h2");
-    showHeading.innerText = show.title;
+    showHeading.innerText = show.titleText;
     const showDetails = document.createElement("p");
     const seasons = document.createElement("p");
-    seasons.innerText = show.numberOfSeasons + " seasons";
-    const ratings = document.createElement("p");
-    ratings.innerText =
-      show.averageRating > 0
-        ? show.ratings.length +
-          " ratings\n" +
-          show.averageRating.toFixed(1) +
-          " stars"
-        : "No ratings yet";
+    seasons.innerText = show.seasonsText;
+    const episodes = document.createElement("p");
+    episodes.innerText = show.episodesText;
     showDetails.append(seasons);
-    showDetails.append(ratings);
+    showDetails.append(episodes);
     showPane.append(showHeading);
     showPane.append(showDetails);
     body.append(showPane);
-  }
-};
-
-const removeShows = () => {
-  const children = [];
-  for (let childNode of body.childNodes) {
-    children.push(childNode);
-  }
-  for (let child of children) {
-    if (child.tagName == "DIV") {
-      body.removeChild(child);
-    }
-  }
-};
-
-const addRandomRatings = () => {
-  for (let show of shows) {
-    if (Math.random() >= 0.2) {
-      const numberOfRatings = Math.floor(Math.random() * 4 + 1);
-      for (let i = 0; i < numberOfRatings; i++) {
-        const rating = Math.floor(Math.random() * 5 + 1);
-        show.addRating(rating);
-      }
-    }
   }
 };
 
