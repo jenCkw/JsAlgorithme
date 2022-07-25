1630c62321932e8c4ac749cc6c82b7aa8df755b7
jenCkw <cikwaninejenny@gmail.com>
2022-07-25 14:40:00 +0100

    modification, ajout, suppression d'un element quelconque avec le DOM

--- script.js
+++ script.js
@@ -1,36 +1,6 @@
-//On pointe sur l'élément de message
-const espaceMessage = document.getElementById("message");
-//On pointe sur l'élément de bouton
-const bouton = document.getElementById("bouton");
-//On pointe sur l'élément de champ de saisie de l'année
-const anneeInput = document.getElementById("annee");
-
-let params = [];
-for (let i = 20; i > 0; i--) {
-  params.push(i);
-}
-
-//On défini la variage année qu'on utilisera et un variable définissant l'age de la majorité
-let annee;
-
-function onConvert() {
-  //On récupère la saisie de l'année et on transforme le texte en nombre entier
-  annee = parseInt(anneeInput.value);
-  //Si la saisie n'est pas un nombre, on affiche un message d'erreur
-  if (isNaN(annee)) {
-    alert("Ceci n'est pas un nombre");
-    return;
-  }
-  const mois = annee * params[12];
-
-  // ANALYSER ICI AVEC CONSOLE LOG
-  // Analyser la variable annee
-  // Analyser la variable params
-  // Analyser la variable mois
-  // FIN ANALYSE
-
-  espaceMessage.innerHTML = annee + " années font " + mois + "mois ";
-}
-
-//On écoute l'action de click sur le onConvert et on appelle la fonction onConvert
-bouton.addEventListener("click", onConvert);
+let p = document.createElement("p");
+const addp = document.getElementById("main");
+addp.appendChild(p);
+p.innerHTML = "Mon <strong>grand</strong> contenu";
+p.classList.add("important");
+p.style.color = "green";
