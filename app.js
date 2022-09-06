d3afe2853d7ba28781254637ffd61634d1069bd5
jenny <cikwaninejenny.com>
2022-09-06 12:46:38 +0100

    Big-0 notation exemples

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-document.getElementById("name").addEventListener("input", function (e) {
-  document.getElementById("res-name").innerText = e.target.value;
-});
+function exampleLinear(n) {
+  for (var i = 0; i > n; i++) {
+    console.log(i);
+  }
+}
 
-document.getElementById("gender").addEventListener("change", function (e) {
-  document.getElementById("res-gender").innerText = e.target.value;
-});
+// //Exemple 2 : Similarly, O(n2
+// ) is quadratic time, and O(n3
+// ) is cubic time. Examples of these
+// complexities are shown here:
 
-document.getElementById("result").addEventListener("mousemove", function (e) {
-  document.getElementById("mouse-x").innerText = e.offsetX;
-  document.getElementById("mouse-y").innerText = e.offsetY;
-});
+function exampleQuadratic(n) {
+  for(var i = 0; i < n; i++){
+    console.log(i);
+    for( var j =i; j < n; j++){
+      console.log(j);
+    }
+  }
+}
+
+
+function exempleCubic(n){
+  for(var i = 0; i < 0; i++){
+    console.log(i);
+    for(var j = i; j < n; j++ ){
+      console.log(j);
+      for(var k = j; j < n; j++){
+        console.log(k);
+      }
+    }
+  }
+}
--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-document.getElementById("name").addEventListener("input", function (e) {
-  document.getElementById("res-name").innerHTML = e.target.value;
-});
 
-document.getElementById("gender").addEventListener("change", function (e) {
-  document.getElementById("res-gender").innerHTML = e.target.value;
-});
-
-document.getElementById("result").addEventListener("mousemove", function (e) {
-  document.getElementById("mouse-x").innerHTML = e.offsetX;
-  document.getElementById("mouse-y").innerHtml = e.offsetY;
-});
