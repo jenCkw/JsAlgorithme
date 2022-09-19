dbd3689e9cb6122d52a0865b501ee872fcdcbd40
jenny <cikwaninejenny.com>
2022-09-19 19:14:43 +0100

    In this example, each property is compared to achieve an accurate object equality result

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-function a(n){
-  var count = 0;
-  for(var i = 0; i<n; i++) {
-    count += 1;
+function isEquivalent (a,b) {
+
+  //arry of proparty names
+
+  var aProps = Object.getOwnPropertyNames(a);
+  var bProps = Object.getOwnPropertyNames(b)
+
+  // if their property lengths are different, they're different objects
+
+  if(aProps.length != bProps.length) {
+    return false;
   }
-  return count;
+
+  for (var i = 0; i < aProps.length; i++) {
+    var propName = aProps[i];
+
+    //if the values of the property are different, not equal
+    if(a[propName != b[propName]]){
+      return false;
+    }
+
+  }
+  return true;
 }
-console.log(count)
+
+  // return true
+
+console.log(isEquivalent({ hi: 13 }, { hi: 12 }));
